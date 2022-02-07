a337abafe0f24b74916416f08953a1cff0b2cca3
JozefTkocz <49951089+JozefTkocz@users.noreply.github.com>
2022-02-07 20:06:27 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def read_requirements(requirements_file):
+    with open(requirements_file) as f:
+        required_packages = f.read().splitlines()
+    return required_packages
+
+
+setup(
+    name="stravaclient",
+    python_requires=">=3.8,<4.0",
+    version="0.0.1",
+    author="Jozef Tkocz",
+    author_email="jozeftkocz@gmail.com",
+    description=("Python client for the Strava API"),
+    packages=find_packages(),
+    install_requires=read_requirements('requirements.txt'),
+    long_description=read('README.md')
+)
